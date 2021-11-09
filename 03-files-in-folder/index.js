bc177001a57d36247448090c0e3ddc472091420a
altynai91 <lenta.kg@gmail.com>
2021-11-09 12:05:09 +0300

    feat: edit indexjs file

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -11,18 +11,7 @@ fs.readdir('03-files-in-folder/secret-folder', {withFileTypes: true}, function(e
         console.log(path.basename(items[i].name, path.extname(items[i].name)) + ' - ' + path.extname(items[i].name).slice(1) + ' - ' + stats.size + 'kb');
       }
       else if (items[i].isDirectory()) {
-        fs.readdir(`03-files-in-folder/secret-folder/${items[i].name}`, {withFileTypes: true}, function(error, itemsin) {
-          for (let k=0; k<itemsin.length; k++) {
-            fs.stat(`03-files-in-folder/secret-folder/${items[i].name}/${itemsin[k].name}`, (error, stats) => {
-              if (error) {
-                console.log(error);
-              }
-              else if (itemsin[k].isFile()) {
-                console.log(path.basename(itemsin[k].name, path.extname(itemsin[k].name)) + ' - ' + path.extname(itemsin[k].name).slice(1) + ' - ' + stats.size + 'kb');
-              }
-            });
-          }
-        });
+        return;
       }
     });
   }
